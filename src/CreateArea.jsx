61962ccf589ed07ec95a47a9910efb636e7d5a26
Maxim Sur <makarka16@gmail.com>
2024-01-24 13:30:00 +0200

    Add styling

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
 
 function CreateArea(props) {
+  
+  const [isTyping, setTyping] = useState(false);
+  const [textRows, setRows] = useState("1");
+
+  function handleFocus() {
+    setTyping(true);
+    setRows("3");
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input onChange={(event) => {props.change(event)}} name="title" placeholder="Title" value={props.title} />
-        <textarea onChange={(event) => {props.change(event)}} name="content" placeholder="Take a note..." value={props.content} rows="3" />
-        <button onClick={(event) => {props.add(event)}}>Add</button>
+        <input 
+        onChange={(event) => { props.change(event) }} 
+        name="title" 
+        placeholder="Title" 
+        value={props.title} 
+        style={{display: isTyping ? "block" : "none"}} 
+        />
+        <textarea 
+        onChange={(event) => { props.change(event) }} 
+        name="content" 
+        placeholder="Take a note..." 
+        value={props.content} 
+        rows={textRows}
+        onFocus={handleFocus} 
+        />
+
+        {isTyping && <Zoom in={true}>
+          <Fab onClick={(event) => { props.add(event) }}>
+            <AddIcon />
+          </Fab>
+        </Zoom>}
       </form>
     </div>
   );
